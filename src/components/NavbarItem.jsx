6ff23ed971dec9fc65849016399822ca218a9bef
sidiqslenses <sidiqslenses@gmail.com>
2024-06-17 13:47:10 +0000

    haha fix navbar & results

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,18 +1,31 @@
 'use client'
 
 import Link from 'next/link'
+import { Suspense } from 'react'; // Tambahkan import Suspense
 import { useSearchParams } from 'next/navigation'
 
-export default function NavbarItem( { title, param} ) {
-  const searchPrams = useSearchParams();
-  const genre = searchPrams.get('genre');
-    return (
+export default function NavbarItem({ title, param }) {
+  return (
+    <Suspense fallback={<div>Loading...</div>}> {/* Tambahkan Suspense di sini */}
+      <NavbarItemContent title={title} param={param} />
+    </Suspense>
+  );
+}
+
+function NavbarItemContent({ title, param }) {
+  const searchParams = useSearchParams();
+  const genre = searchParams.get('genre');
+  
+  return (
     <div>
-        <Link 
+      <Link 
         className={`hover:text-amber-600 font-semibold
         ${genre === param ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg' : ''}
         `}
-        href={`/?genre=${param}`}>{title}</Link>
+        href={`/?genre=${param}`}
+      >
+        {title}
+      </Link>
     </div>
-  )
+  );
 }
