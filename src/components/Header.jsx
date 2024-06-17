da59fc5d90e98962ac30ebd127ce9d68cc38480e
sidiqslenses <sidiqslenses@gmail.com>
2024-06-17 02:13:22 +0000

    dark mode switch

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {AiFillHome} from 'react-icons/ai'
+import MenuItem from './MenuItem'
+import {BsFillInfoCircleFill} from 'react-icons/bs'
+import Link from 'next/link'
+import DarkModeSwitch from './DarkModeSwitch'
+
+export default function Header() {
+  return (
+    <div className='flex justify-between items-center p-3 m-w-6xl'>
+        <div className='flex gap-4'>
+          <MenuItem title="home" address='/' Icon={AiFillHome} />
+          <MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill} />
+        </div>
+        <div className='flex items-center gap-4'>
+          
+        <DarkModeSwitch />
+          
+        <Link href={'/'} className='flex gap-1 items-center'>
+          <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>IMDb</span>
+          <span className='text-xl hidden sm:inline'>Clone</span>
+        </Link>
+        </div>
+     
+    </div>
+  )
+}
