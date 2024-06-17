2d00bf9bccc0de4d17661b499a91e5a629c9030d
sidiqslenses <sidiqslenses@gmail.com>
2024-06-17 07:55:16 +0000

    new page, movie page, search page

--- src/app/page.js
+++ src/app/page.js
@@ -4,21 +4,33 @@ const API_KEY = process.env.API_KEY;
 
 export default async function Home({ searchParams }) {
   const genre = searchParams.genre || 'fetchTrending';
-  const res = await fetch(
-    `https://api.themoviedb.org/3${
-      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
-    }?api_key=${API_KEY}&language=en-US&page=1`,
-    { next: { revalidate: 10000 } }
-  );
-  const data = await res.json();
-  if (!res.ok) {
-    throw new Error('Failed to fetch data');
-  }
-  const results = data.results;
+  let url;
 
-  return (
-    <div>
-      <Results results={results} />
-    </div>
-  );
+  if (genre === 'fetchTopRated') {
+    url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`;
+  } else {
+    url = `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US&page=1`;
+  }
+
+  try {
+    const res = await fetch(url, { next: { revalidate: 10000 } });
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    const data = await res.json();
+    const results = data.results;
+
+    return (
+      <div>
+        <Results results={results} />
+      </div>
+    );
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return (
+      <div className="text-center mt-10">
+        <h1>Something went wrong. Please try again later.</h1>
+      </div>
+    );
+  }
 }
