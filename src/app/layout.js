f0d73afbbf3de92b08a694d43a3909467093fc43
sidiqslenses <sidiqslenses@gmail.com>
2024-06-17 07:34:01 +0000

    add search box

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,28 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Header from "@/components/Header";
-import Providers from "./Providers";
-import Navbar from "@/components/Navbar";
-// import SearchBox from "@/components/SearchBox";
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Header from '@/components/Header';
+import Providers from './Providers';
+import Navbar from '@/components/Navbar';
+import SearchBox from '@/components/SearchBox';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "IMDb clone",
-  description: "This is movie database clone",
+  title: 'IMDb clone',
+  description: 'This is a movie database clone',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>
-        <Providers defaultTheme='system' attribute='class'>
-        <Header />
-        <Navbar />
-        {/* <SearchBox /> */}
-        {children}
+        <Providers>
+          <Header />
+          <Navbar />
+          <SearchBox />
+          {children}
         </Providers>
-        </body>
+      </body>
     </html>
   );
 }
